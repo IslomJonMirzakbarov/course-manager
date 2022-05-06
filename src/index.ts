3b8289b25e566cd551ace304825266a4ae31c8d1
IslomJonMirzakbarov <islomjonmirzakbarov@gmail.com>
2022-05-06 15:49:37 +0900

    saved documenst into mongodb cloud

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,11 @@ const mongoose = require("mongoose");
 const uri =
   "mongodb+srv://islom:islom123@mosh-node-app.6twhk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
+mongoose
+  .connect(uri)
+  .then(() => console.log("Connected to MongoDb..."))
+  .catch((err: any) => console.log(err));
+
 const courseSchema = new mongoose.Schema({
   name: String,
   author: String,
@@ -13,14 +18,17 @@ const courseSchema = new mongoose.Schema({
 
 // creating class model and its instance
 const Course = mongoose.model("Course", courseSchema);
-const course = new Course({
-  name: "Nodejs",
-  author: "Islom",
-  tags: ["nodejs", "backend"],
-  isPublished: true,
-});
 
-mongoose
-  .connect(uri)
-  .then(() => console.log("Connected to MongoDb..."))
-  .catch((err: any) => console.log(err));
+const createCourse = async () => {
+  const course = new Course({
+    name: "react",
+    author: "Islom",
+    tags: ["react", "frontend"],
+    isPublished: true,
+  });
+
+  const result = await course.save();
+  console.log(result);
+};
+
+createCourse();
